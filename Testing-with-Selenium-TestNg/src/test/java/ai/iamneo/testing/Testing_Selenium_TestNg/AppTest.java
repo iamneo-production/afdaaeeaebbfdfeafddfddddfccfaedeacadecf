4bf4099582d0316e9cd918485cff0cb68deef7b2
Iamneo <techadmin@iamneo.ai>
2023-08-13 17:08:40 +0000

    Backup Commit

--- Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
+++ Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -25,35 +25,49 @@ public class AppTest {
 //Checking the title of iamNeo (Home - iamneo)
 	public void iamNeo() throws InterruptedException 
 	{
-		 driver.get("http://www.fb.com");
-		 String title ="Get your title";
-		Assert.assertEquals(title, "");
-	}
-	@Test
-//Moving to FACEBOOK
-	public void nextPage() throws InterruptedException 
-	{
-		 	
-		 String title ="Get your title";
-		Assert.assertEquals(title, "");
+		driver.get("http://www.fb.com");
+
+        String currentUrl = driver.getCurrentUrl();
+        if (currentUrl.equals("http://www.facebook.com")) {
+            System.out.println("URL redirected correctly.");
+        } else {
+            System.out.println("URL redirection failed.");
+        }
+
+        WebElement createAccountSection = driver.findElement(By.id("createAccountSection"));
+        if (createAccountSection.isDisplayed()) {
+            System.out.println("Create an account section is present.");
+        } else {
+            System.out.println("Create an account section is not present.");
+        }
+
+        driver.findElement(By.name("firstname")).sendKeys("John");
+        driver.findElement(By.name("lastname")).sendKeys("Doe");
+        driver.findElement(By.name("reg_email__")).sendKeys("example@example.com");
+        driver.findElement(By.name("reg_email_confirmation__")).sendKeys("example@example.com");
+        driver.findElement(By.name("reg_passwd__")).sendKeys("password123");
+
+        Select dayDropdown = new Select(driver.findElement(By.id("day")));
+        dayDropdown.selectByValue("15");
+
+        Select monthDropdown = new Select(driver.findElement(By.id("month")));
+        monthDropdown.selectByValue("6");
+
+        Select yearDropdown = new Select(driver.findElement(By.id("year")));
+        yearDropdown.selectByValue("1990");
+
+        driver.findElement(By.cssSelector("input[value='2']")).click();
+
+        driver.findElement(By.name("websubmit")).click();
+
+        if (driver.getCurrentUrl().contains("https://www.facebook.com/welcome")) {
+            System.out.println("Account created successfully.");
+        } else {
+            System.out.println("Account creation failed.");
+        }
 
 	}
-	@Test
-//Back to iamNeo
-	public void backPage() throws InterruptedException 
-	{
-		 String title ="Get your title";
-		Assert.assertEquals(title, "");
-
-	}
-	@Test
-//Current URL
-public void currentURL() throws InterruptedException 
-{
-		 String title ="Get your title";
-		Assert.assertEquals(title, "");
-
-}
+	
 
 	@AfterTest
 	public void afterTest() 
